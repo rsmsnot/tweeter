2618a0cd85af2db401ab605adb582029819117c1
rsmsnot <rsmsnot@outlook.com>
2019-06-11 20:55:09 +0000

    character counter turns red when over 140 limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,18 @@
 
 $(document).ready(function() {
     // console.log("testing");
-    let characterLimit = 140;
+    const characterLimit = 140;
 
     $('.new-tweet textarea').on("keyup", function(){
        let counter = $(this).siblings(".counter");
        let characterCount = $(this).val().length;
        let charactersRemaining = characterLimit - characterCount;
     //    console.log( characterCount, charactersRemaining);
-       $(counter).text(charactersRemaining);
-
-
+       counter.text(charactersRemaining);
+       if(characterCount > characterLimit){
+            counter.css( "color", "red")
+       } else {
+        counter.css("color", "")
+            }
+       });
     });
-  });
