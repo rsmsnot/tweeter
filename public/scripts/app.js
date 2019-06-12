7b3543943452e8b0d65e11b911eae1c5edd23eab
rsmsnot <rsmsnot@outlook.com>
2019-06-12 03:38:07 +0000

    implemented createTweetElement function and the renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,115 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// calculate time ago
+function timeAgo(ts) {
+    const d = new Date();
+    const nowTs = Math.floor(d.getTime() / 1000);
+    const seconds = nowTs - ts;
 
+    if (seconds > 2 * 24 * 3600) {
+        return "a few days ago";
+    }
+    if (seconds > 24 * 3600) {
+        return "yesterday";
+    }
+
+    if (seconds > 3600) {
+        return "a few hours ago";
+    }
+    if (seconds > 1800) {
+        return "Half an hour ago";
+    }
+    if (seconds > 60) {
+        return Math.floor(seconds / 60) + " minutes ago";
+    }
+    return "A long time ago"
+}
+
+
+
+$(document).ready(function () {
+    function createTweetElement(tweetData) {
+
+        var newTweet = $("<article>").addClass("tweet");
+
+        let header = $("<header>").appendTo(newTweet);
+        $("<img>")
+            .addClass("avatar")
+            .attr("src", tweetData.user.avatars.small)
+            .appendTo(header);
+        $("<div>")
+            .addClass("username")
+            .text(tweetData.user.name)
+            .appendTo(header);
+        $("<div>")
+            .addClass("handle")
+            .text(tweetData.user.handle)
+            .appendTo(header);
+
+        let mainText = $("<p>").text(tweetData.content.text).appendTo(newTweet);
+
+        let footer = $('<footer>').text(timeAgo(tweetData.created_at)).appendTo(newTweet);
+
+        return newTweet;
+    }
+
+    const tweetData = [
+        {
+            "user": {
+                "name": "Newton",
+                "avatars": {
+                    "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+                    "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+                    "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+                },
+                "handle": "@SirIsaac"
+            },
+            "content": {
+                "text": "If I have seen further it is by standing on the shoulders of giants"
+            },
+            "created_at": 1461116232227
+        },
+        {
+            "user": {
+                "name": "Descartes",
+                "avatars": {
+                    "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+                    "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+                    "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+                },
+                "handle": "@rd"
+            },
+            "content": {
+                "text": "Je pense , donc je suis"
+            },
+            "created_at": 1461113959088
+        },
+        {
+            "user": {
+                "name": "Johann von Goethe",
+                "avatars": {
+                    "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+                    "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+                    "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+                },
+                "handle": "@johann49"
+            },
+            "content": {
+                "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+            },
+            "created_at": 1461113796368
+        }
+    ];
+
+
+
+    function renderTweets(tweets) {
+        for (let people of tweets) {
+            $('.tweet-container').append(createTweetElement(people));
+        }
+    }
+    renderTweets(tweetData);
+
+
+});
